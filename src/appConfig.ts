1802f4f64b0227ba89f53a357d416380cc2ab65f
1esse <572116385@qq.com>
2022-06-24 09:34:06 +0800

    update

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -6,9 +6,16 @@ export const appTitle = 'ClownFish Admin'
 /**
  * 使用mock代理api请求：on开，off关  
  */
-export const mock: 'on' | 'off' = 'on'
+export const mock: SwitchType = 'on'
 
 /**
  * mock是否开启namespace，开启后文件名将作为前缀拼接在url
  */
 export const mockNamespace: boolean = true
+
+/**
+ * mock代理指定环境
+ * 只在开发环境且appConfig的mock字段为‘on’的情况启动mock，
+ * 由于线上预览需要，这里不限制环境
+ */
+export const mockEnv: envType[] = ['development', 'staging', 'production']
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import { mock } from './appConfig'
+import { mock, mockEnv } from './appConfig'
 import enableMock from './mock'
 import 'ant-design-vue/es/message/style/index.css' // antdv message样式
 import '@/styles/index.postcss' // 全局样式
 import 'virtual:svg-icons-register'
 import './permission'
 
-// 只在开发环境且appConfig的mock字段为‘on’的情况启动mock，
-// 由于线上预览需要，这里不限制环境
-['development', 'staging', 'production'].includes(import.meta.env.MODE) && mock === 'on' && enableMock()
+mockEnv.includes(import.meta.env.MODE as envType) && mock === 'on' && enableMock()
 
 createApp(App).use(createPinia()).use(router).mount('#app')
