e783fb40c4c77a6b9f232f1d708704fff79bc478
1esse <572116385@qq.com>
2022-06-30 11:56:52 +0800

    update

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,14 @@
-declare namespace AppGlobal {
-  /**
-   * 开关类型：on开 off关
-   */
-  type SwitchType = 'on' | 'off'
 
-  /**
-   * 环境类型 
-   * 
-   * development：开发环境；
-   * staging：预发布环境；
-   * production：生产环境；
-   */
-  type EnvType = 'development' | 'staging' | 'production'
-}
+/**
+ * 开关类型：on开 off关
+ */
+export declare type SwitchType = 'on' | 'off'
+
+/**
+ * 环境类型 
+ * 
+ * development：开发环境；
+ * staging：预发布环境；
+ * production：生产环境；
+ */
+export declare type EnvType = 'development' | 'staging' | 'production'
