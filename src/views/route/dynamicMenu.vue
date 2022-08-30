0a54d742496318b6a1656698c1df0de101cb6a83
1esse <572116385@qq.com>
2022-08-30 21:26:42 +0800

    update

--- src/views/route/dynamicMenu.vue
+++ src/views/route/dynamicMenu.vue
@@ -4,7 +4,7 @@ export default { name: 'DynamicMenu' }
 <script lang="ts" setup>
 import { routes } from '@/router'
 import { sidebarStore } from '@/stores/sidebar'
-import { FireOutlined, MenuOutlined } from '@ant-design/icons-vue'
+import { MenuOutlined } from '@ant-design/icons-vue'
 import { defineComponent, h, ref } from 'vue'
 import { RouteRecordRaw, useRouter } from 'vue-router'
 
@@ -14,7 +14,7 @@ const sidebar = sidebarStore()
 
 function addDynamicMenu(keepAlive: boolean = false) {
   count.value++
-  const path = '/newRoute' + count.value
+  const path = '/newMenu' + count.value
   const parentRoute = routes.find(route => route.name === 'Route')
   if (!parentRoute) return
   const dynamicRoute = parentRoute?.children?.find(item => item.path === path)
@@ -24,14 +24,14 @@ function addDynamicMenu(keepAlive: boolean = false) {
   }
   const comp = defineComponent({
     props: ['id'],
-    name: 'newRoute' + count.value,
+    name: 'newMenu' + count.value,
     render() {
-      return h('div', `这里是新增的菜单${'newRoute' + this.$props.id}页面`)
+      return h('div', `这里是新增的菜单${'newMenu' + this.$props.id}页面`)
     }
   })
   const newRoute: RouteRecordRaw = {
     path: path,
-    name: 'newRoute' + count.value,
+    name: 'newMenu' + count.value,
     component: comp,
     meta: { title: `${'新增菜单' + count.value}`, icon: MenuOutlined, keepAlive: keepAlive },
     props: { id: count.value }
@@ -45,7 +45,7 @@ function addDynamicMenu(keepAlive: boolean = false) {
 
 <template>
   <div>
-    <p>已创建动态菜单数：{{  count  }}</p>
+    <p>已创建动态菜单数：{{ count }}</p>
     <ASpace>
       <AButton @click="() => addDynamicMenu()">新增动态菜单</AButton>
       <AButton @click="() => addDynamicMenu(true)">新增动态菜单（缓存）</AButton>
