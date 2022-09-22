1b79a93c8bb5e0343dcc17c13c55870be18352c5
zhao-jiexin <zhaojiexin@hongjianguo.com>
2022-09-22 18:02:20 +0800

    update

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,48 +1,4 @@
 <script setup lang="ts">
-import { message, Modal } from 'ant-design-vue'
-import { h, reactive } from 'vue'
-
-const visibles = reactive({
-  dialog1: false,
-  dialog2: false
-})
-const loading = reactive({
-  dialog1: false
-})
-const loadingText = reactive({
-  dialog1: ''
-})
-const state = reactive({
-  height: '',
-  plus1: 0,
-  plus2: 0,
-  minus1: 0,
-  minus2: 0,
-  accMul1: 0,
-  accMul2: 0,
-  accDiv1: 0,
-  accDiv2: 1
-})
-
-function calcHeight() {
-  if (!state.height) return message.warn('请输入您的身高')
-  loading.dialog1 = true
-  loadingText.dialog1 = '正在努力计算中，请稍等...'
-  setTimeout(() => {
-    loading.dialog1 = false
-    visibles.dialog1 = false
-    message.success(`经过大量复杂的计算，已确定您的身高是${state.height}cm!`, 5)
-  }, 2000)
-}
-
-function deviceTest() {
-  Modal.success({
-    title: '诊断结果',
-    content: h('div', {}, [
-      h('p', '经诊断，您的设备可正常开机！'),
-    ]),
-  })
-}
 </script>
 
 <template>
