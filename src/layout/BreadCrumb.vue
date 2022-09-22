1c47ae4fd2b67ff0654b5875e3eb67d39df2b498
zhao-jiexin <zhaojiexin@hongjianguo.com>
2022-09-22 15:52:05 +0800

    优化面包屑

--- src/layout/BreadCrumb.vue
+++ src/layout/BreadCrumb.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { nextTick, onBeforeMount, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { nextTick, onBeforeMount, shallowRef, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import type { RouteLocationMatched } from 'vue-router'
 import { dashboardRoute } from '@/router'
 import { resolve } from 'pathe'
 
+const router = useRouter()
 const route = useRoute()
-const routeMatched = ref<RouteLocationMatched[]>([])
+const allRoutes = router.getRoutes()
+const routeMatched = shallowRef<RouteLocationMatched[]>([])
 const props = withDefaults(defineProps<{
   withIcons?: boolean
 }>(), {
@@ -19,7 +21,16 @@ watch(() => route.path, refreshBreadCrumb)
 function refreshBreadCrumb() {
   routeMatched.value = []
   nextTick(() => {
+    const options = []
     routeMatched.value = route.matched.filter((item) => item.meta.breadcrumb !== false)
+    for (const matched of routeMatched.value) {
+      if (matched.meta.belongs) {
+        const belongRoute = allRoutes.find(item => item.path === matched.meta.belongs)
+        belongRoute && options.push(belongRoute)
+      }
+      options.push(matched)
+    }
+    routeMatched.value = options
     if (routeMatched.value.length === 0) {
       routeMatched.value.unshift({ ...dashboardRoute.children?.[0], path: resolve('/', dashboardRoute.children?.[0].path as string) } as RouteLocationMatched)
       return
@@ -30,7 +41,7 @@ function refreshBreadCrumb() {
   })
 }
 </script>
-
+  
 <template>
   <Transition name="fade-scale" mode="out-in" appear>
     <ABreadcrumb v-if="routeMatched.length > 0">
@@ -47,3 +58,4 @@ function refreshBreadCrumb() {
     </ABreadcrumb>
   </Transition>
 </template>
+  
